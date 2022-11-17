26abd01476b01d7b1d6ce9305ba4d8a0ab3c7d2f
Sai Kiran Gajjala <saikiran.n73@gmail.com>
2022-11-16 22:41:53 -0500

    feat: added apollo federation support

--- src/main/java/com/rrajesh1979/graphqltechtalk/config/GraphQLConfig.java
+++ src/main/java/com/rrajesh1979/graphqltechtalk/config/GraphQLConfig.java
@@ -2,6 +2,8 @@ package com.rrajesh1979.graphqltechtalk.config;
 
 import graphql.scalars.ExtendedScalars;
 import graphql.schema.GraphQLScalarType;
+import com.apollographql.federation.graphqljava.Federation;
+import org.springframework.boot.autoconfigure.graphql.GraphQlSourceBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.graphql.execution.RuntimeWiringConfigurer;
@@ -11,6 +13,16 @@ public class GraphQLConfig {
     @Bean
     public RuntimeWiringConfigurer runtimeWiringConfigurer() {
         return wiringBuilder -> wiringBuilder.scalar(ExtendedScalars.Date);
+    }
+
+    @Bean
+    public GraphQlSourceBuilderCustomizer federationTransform() {
+        return builder -> {
+            builder.schemaFactory((registry, wiring) -> Federation.transform(registry, wiring)
+                    .fetchEntities(env -> null)
+                    .resolveEntityType(env -> null)
+                    .build());
+        };
     }
 
 }
