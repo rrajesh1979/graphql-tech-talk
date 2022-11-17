74d755918223fa3db2fc3c65c2c8bacde3d20da8
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2022-11-17 11:30:37 -0500

    Apollo Rover Integration

--- src/main/java/com/rrajesh1979/graphqltechtalk/config/GraphQLConfig.java
+++ src/main/java/com/rrajesh1979/graphqltechtalk/config/GraphQLConfig.java
@@ -4,12 +4,24 @@ import graphql.scalars.ExtendedScalars;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+import com.apollographql.federation.graphqljava.Federation;
+import org.springframework.boot.autoconfigure.graphql.GraphQlSourceBuilderCustomizer;
 
 @Configuration
 public class GraphQLConfig {
     @Bean
     public RuntimeWiringConfigurer runtimeWiringConfigurer() {
         return wiringBuilder -> wiringBuilder.scalar(ExtendedScalars.Date);
+    }
+
+    @Bean
+    public GraphQlSourceBuilderCustomizer federationTransform() {
+        return builder -> {
+            builder.schemaFactory((registry, wiring) -> Federation.transform(registry, wiring)
+                    .fetchEntities(env -> null)
+                    .resolveEntityType(env -> null)
+                    .build());
+        };
     }
 
 }
