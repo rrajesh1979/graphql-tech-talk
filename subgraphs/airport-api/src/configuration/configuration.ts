b791dd90e79d263c573d227d1a6fbd2539773b3a
Sai Kiran Gajjala <saikiran.n73@gmail.com>
2022-11-17 10:33:14 -0500

    Nestjs api port change

--- subgraphs/airport-api/src/configuration/configuration.ts
+++ subgraphs/airport-api/src/configuration/configuration.ts
@@ -1,6 +1,4 @@
 export default () => ({
-  env: process.env.APP_ENV || 'local',
-  port: parseInt(process.env.APP_PORT, 10) || 8080,
-  keyVaultUrl: process.env.KEYVAULT_URL || 'employerportal-kv',
+  port: 8081,
   mongoConnectionString: process.env.MONGO_CONNECTION_STRING
 });
