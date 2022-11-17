6d430f015fd25cd02740d9cc18af67d845640b89
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2022-11-17 16:41:42 -0500

    🧪Adding GraphQL Tests

--- src/test/java/com/rrajesh1979/graphqltechtalk/controller/CrewMemberIntTest.java
+++ src/test/java/com/rrajesh1979/graphqltechtalk/controller/CrewMemberIntTest.java
@@ -2,6 +2,7 @@ package com.rrajesh1979.graphqltechtalk.controller;
 
 import com.rrajesh1979.graphqltechtalk.model.CrewMemberDoc;
 import com.rrajesh1979.graphqltechtalk.repository.CrewMemberMongoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,7 @@ import org.springframework.graphql.test.tester.GraphQlTester;
 @GraphQlTest(CrewMemberController.class)
 @Import(CrewMemberMongoRepository.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Slf4j
 public class CrewMemberIntTest {
     @Autowired
     GraphQlTester graphQlTester;
@@ -34,6 +36,8 @@ public class CrewMemberIntTest {
             }
         }            
         """;
+
+        log.info("document: {}", document);
 
         graphQlTester.document(document)
                 .execute()
