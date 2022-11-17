6f69c65a8198f46bd3c0ccce5da8194c9850ba50
Sai Kiran Gajjala <saikiran.n73@gmail.com>
2022-11-17 14:49:43 -0500

    router commands

--- supergraph.sh
+++ supergraph.sh
@@ -1,2 +1,8 @@
 rover dev --name airport --schema ~/Learn/graphql/graphql-tech-talk/subgraphs/airport-api/src/schema/schema.gql --url http://localhost:8081/graphql
 rover dev --name crew --schema ~/Learn/graphql/graphql-tech-talk/src/main/resources/graphql/schema.graphqls --url http://localhost:8080/graphql
+
+#command to generate super graph schema file
+rover supergraph compose --config ./supergraph.yaml > supergraph-schema.graphql
+
+#command to run the router using the above generated schema file
+./router --dev --supergraph supergraph-schema.graphql
