20d38a4b64e5b27cbabc147e175f64618ece8003
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2022-11-17 15:22:01 -0500

    Apollo Rover Integration

--- supergraph.sh
+++ supergraph.sh
@@ -1,3 +1,9 @@
+# Install Apollo RoverCLI
+
+
+# Install Apollo Router
+curl -sSL https://router.apollo.dev/download/nix/latest | sh
+
 rover dev --name airport --schema ~/Learn/graphql/graphql-tech-talk/subgraphs/airport-api/src/schema/schema.gql --url http://localhost:8081/graphql
 rover dev --name crew --schema ~/Learn/graphql/graphql-tech-talk/src/main/resources/graphql/schema.graphqls --url http://localhost:8080/graphql
 
